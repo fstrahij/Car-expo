d3b86fb96c1d9a4ed4f30969054f2804f5e54c28
Filip Strahija <filip2893@gmail.com>
2019-02-23 12:11:07 +0100

    navbar created

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get '/expoes', to: 'expos#index'
   get '/signup', to: 'users#new'
   get '/about', to: 'static_pages#about'
+  get '/home', to: 'static_pages#home'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
