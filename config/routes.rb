280cf78bb94ac008cdbaa84dee99b992567cd2aa
Filip Strahija <filip2893@gmail.com>
2019-02-25 15:45:55 +0100

    basic login with log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
 
   resources :expos
+  resources :users
   root 'static_pages#home'
   get '/expoes', to: 'expos#index'
   get '/signup', to: 'users#new'
   get '/about', to: 'static_pages#about'
   get '/home', to: 'static_pages#home'
-  resources :users
+  get '/login', to: 'sessions#new'  
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
