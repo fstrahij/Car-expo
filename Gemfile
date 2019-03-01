4a8e64f90ae530fc55cda38342123b905184c3d3
fstrahij <filip2893@gmail.com>
2019-03-01 18:47:58 +0100

    faker added

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'bcrypt', '~> 3.1.7'
 
 gem 'bootstrap-sass', '3.4.1'
+
+gem 'faker', '~> 1.9'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
- User.create(first_name: "Miran", 
- 			last_name: "Maras", 
- 			user_name: "miric123", 
- 			email: "miric@gmail.com", 
- 			password: "miri123", 
- 			password_confirmation: "miri123")
+ 99.times do |n|
+ 	first_name = Faker::Name.first_name
+ 	last_name = Faker::Name.last_name
+ 	user_name = Faker::Internet.user_name
+ 	user_name = "#{user_name}_#{n}"
+ 	email = Faker::Internet.email
+ 	password = "123456"
+ 	User.create(first_name: first_name, 
+ 			last_name: last_name, 
+ 			user_name: user_name, 
+ 			email: email, 
+ 			password: password, 
+ 			password_confirmation: password)
+ end
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
